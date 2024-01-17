4bf84fe19bd50c7c9c067777dcde65aa15a4c961
Fight <chalanthon.ky@gmail.com>
2024-01-17 22:42:52 +0700

    4hr

--- basic01.c
+++ basic01.c
@@ -1,3 +1,27 @@
-void main() {
-    
+#include<stdio.h>
+
+void myFunction(char name[], int age){
+    printf("%s.%d\n", name, age);
 }
+
+    int main() {
+    char r;
+        printf ("Enter Name: " ) ;
+        scanf ("%c", &r) ;
+        // printf ("Name = %s\n");
+    int k;
+        printf ("Enter Age: " ) ;
+        scanf ("%d", &k) ;
+    // int old;
+    // scanf ("%d", &old);
+    if (k <= 50) {
+        printf("Just a kid");
+    } else if (k >= 50){
+        printf("Danm old man");
+    } else {
+        printf("try again");
+    }
+}
+    
+// char array = %s
+// int array = %d
